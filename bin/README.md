639459298680b149912d851ee382a6da72ccc1a7
Mike Tesliuk <mike@tesliuk.com>
2016-11-17 21:34:43 -0200

    BKP script updated and configuration

---
+++ bin/README.md
@@ -0,0 +1,8 @@
+This is script need a .pgpass configuration under root directory, pelase, create a file like this 
+for your root (or the user who will run the script)
+
+ hostname:port:database:username:password
+
+The documentation about this procedure can be found under https://www.postgresql.org/docs/current/static/libpq-pgpass.html
+
+
--- etc/CONFIG.sh
+++ etc/CONFIG.sh
@@ -6,6 +6,17 @@ BKP_DIR="/backup/data/"
 RSYNC_HOST="192.168.1.50"
 RSYNC_USER="backup"
 RSYNC_SECRET_FILE="/backup/etc/rsync.secret"
+RSYNC_REMOTE_MODULE="backup"
+RSYNC_DB_FOLDER="bases"
+RSYNC_GED_FOLDER="backup_files/data/ged"
+RSYNC_DIFF_FOLDER="backup_files/diff/ged"
+RSYNC_LOG="/tmp/rsync-log-`date +%Y.%m.%d`"
 
 ALFRESCO_SCRIPT="/opt/alfresco-community/alfresco.sh"
+PSQL_BIN="/optalfresco-community/postgres/bin/psql"
+PGDUMP_BIN="/optalfresco-community/postgres/bin/psql"
+PG_DATABASE="alfresco"
+
+# File to be used to check if the rsync is correctly configured
+USB_CONTROL_FILE="control"
 

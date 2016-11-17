f707b2b96b1dfa9bad04f41af91f08fc747f3441
Mike Tesliuk <mike@tesliuk.com>
2016-11-17 13:51:09 -0200

    Add etc directory to store configuration files

---
+++ etc/CONFIG.sh
@@ -0,0 +1,8 @@
+# This file should be used to define
+# variables that should be used by the main script
+
+BKP_DIR="/backup/data/"
+
+RSYNC_HOST="192.168.1.50"
+RSYNC_USER="backup"
+RSYNC_SECRET_FILE="/backup/etc/

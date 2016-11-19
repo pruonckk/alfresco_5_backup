674bc80f467f5568c3e0d70e4b3bc27c3f4e4dff
Mike Tesliuk <mike@tesliuk.com>
2016-11-18 22:14:34 -0200

    Added time information on each echo

--- bin/alfresco_bkp.sh
+++ bin/alfresco_bkp.sh
@@ -6,7 +6,7 @@
 # Prevent script to run without a configuration file
 if [ ! -e "../etc/CONFIG.sh" ]; then
 
-	echo "Sorry, you have not read our README.md file, please, check your installation"
+	echo "[$(date "+%d.%m.%Y %H.%M.%S")]: Sorry, you have not read our README.md file, please, check your installation"
 	exit 1
 
 fi
@@ -16,20 +16,20 @@ source ../etc/CONFIG.sh
 
 # We need to stop alfresco as recomended by the documentation
 
-echo "Stopping tomcat"
+echo "[$(date "+%d.%m.%Y %H.%M.%S")]:  Stopping tomcat"
 
 $ALFRESCO_SCRIPT stop tomcat
 
 
 # Starting the Postgres backup
 
-echo "Dumping the database"
+echo "[$(date "+%d.%m.%Y %H.%M.%S")]: Dumping the database"
 
 $PGDUMP_BIN -U postgres -Fc $PG_DATABASE -f $BKP_DIR/$PG_DATABASE-`date +%Y.%m.%d`.dbc 
 
 # Copying Postgresql Backup
 
-echo "Start rsync copy to $RSYNC_REMOTE"
+echo "[$(date "+%d.%m.%Y %H.%M.%S")]: Start rsync copy to $RSYNC_REMOTE"
 
 rsync -av --password-file=$RSYNC_SECRET_FILE  $BKP_DIR/* $RSYNC_USER@$RSYNC_HOST::$RSYNC_REMOTE_MODULE/$RSYNC_DB_FOLDER
 
@@ -41,7 +41,7 @@ rsync -av --password-file=$RSYNC_SECRET_FILE  $BKP_DIR/* $RSYNC_USER@$RSYNC_HOST
 
 # Starting rsync of ged
 
-echo "rsync -av --password-file=$RSYNC_SECRET_FILE  $ALFRESCO_ROOT_DIR/* --backup-dir=$RSYNC_DIFF_FOLDER $RSYNC_USER@$RSYNC_HOST::$RSYNC_REMOTE_MODULE/$RSYNC_GED_FOLDER"
+echo "[$(date "+%d.%m.%Y %H.%M.%S")]: rsync -av --password-file=$RSYNC_SECRET_FILE  $ALFRESCO_ROOT_DIR/* --backup-dir=$RSYNC_DIFF_FOLDER $RSYNC_USER@$RSYNC_HOST::$RSYNC_REMOTE_MODULE/$RSYNC_GED_FOLDER"
 
 # TODO : check the way to do the diff on the remote server, below you have the entry with the backdir
 # rsync -av --password-file=$RSYNC_SECRET_FILE  $ALFRESCO_ROOT_DIR/* --backup-dir=$RSYNC_DIFF_FOLDER $RSYNC_USER@$RSYNC_HOST::$RSYNC_REMOTE_MODULE/$RSYNC_GED_FOLDER
@@ -49,11 +49,11 @@ echo "rsync -av --password-file=$RSYNC_SECRET_FILE  $ALFRESCO_ROOT_DIR/* --backu
 # Right now we are doing backups without the DIFF
 rsync -av --password-file=$RSYNC_SECRET_FILE --delete  $ALFRESCO_ROOT_DIR/* $RSYNC_USER@$RSYNC_HOST::$RSYNC_REMOTE_MODULE/$RSYNC_GED_FOLDER
 
-echo "Sarting tomcat"
+echo "[$(date "+%d.%m.%Y %H.%M.%S")]: Sarting tomcat"
 
 $ALFRESCO_SCRIPT start tomcat
 
-echo "Starting to cleanup the postgres backup"
+echo "[$(date "+%d.%m.%Y %H.%M.%S")]: Starting to cleanup the postgres backup"
 
 if [ -d $BKP_DIR ]; then
 	find $BKP_DIR/ -name "*.dbc" -ctime +$PG_MAX_DAYS -exec rm -vrf {} \; 
